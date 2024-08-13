8e5714c58b8fdd332923164362bc20d815e4f642
Musariaz <riazmusa69@gmail.com>
2024-08-13 15:51:28 +0500

    Setup the Project
    Set up the project and completed the Header Section

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+const App = () => {
+  return (
+    <>
+
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { FaSearch } from "react-icons/fa";
+import {NavLink, Link} from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="bg-slate-300 shadow-xl  ">
+      <div className="flex justify-between items-center  mx-auto p-4">
+        <h1 className="text-bold  sm:text-xl flex flex-wrap">
+          <Link to="/">
+          <span className="text-slate-600 text-4xl">Urban</span>
+          <span className="text-slate-700 text-4xl">Nest</span>
+          </Link>
+        </h1>
+    
+        <ul className="flex gap-5 font-bold text-[22px]  items-center">
+        <form className="bg-slate-200 p-3 rounded-xl flex justify-evenly items-center">
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+          <FaSearch className="text-slate-400"/>
+        </form>
+          <NavLink className="hidden sm:inline hover:underline hover:cursor-pointer " to="/">Home</NavLink>
+          <NavLink className="hidden sm:inline hover:underline hover:cursor-pointer " to ="/about">About</NavLink>
+          <NavLink className=" sm:inline hover:underline hover:cursor-pointer "  to="signin">Sign in</NavLink>
+        </ul>
+      </div>
+    </header>
+
+  );
+};
+
+export default Header;
