31095892a4037906409dbe198a9a00699c6a817d
Musariaz <riazmusa69@gmail.com>
2024-08-22 19:16:47 +0500

    Worked on the navbar search functionality

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,30 @@
 import { FaSearch } from "react-icons/fa";
 import {NavLink, Link} from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useNavigate} from 'react-router-dom'
 const NavBar = () => {
-
+  const [search, setSearch] = useState();
+  const navigate = useNavigate();
   const {user} = useSelector((state) => state.user);
+
+  const handleSearch = (e) =>{
+    e.preventDefault();
+    const url = new URLSearchParams(window.location.search);
+    url.set('searchTerm', search);
+    const searchQuery = url.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(()=>{
+
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearch(searchTermFromUrl);
+    }
+
+  }, [location.search])
 
   return (
     <header className="bg-slate-300 shadow-xl  ">
@@ -16,9 +37,9 @@ const NavBar = () => {
         </h1>
     
         <ul className="flex gap-5 font-bold text-[22px]  items-center">
-        <form className="bg-slate-200 p-3 rounded-xl flex justify-evenly items-center">
-          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
-          <FaSearch className="text-slate-400"/>
+        <form onSubmit={handleSearch} className="bg-slate-200 p-3 rounded-xl flex justify-evenly items-center">
+          <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+          <button><FaSearch className="text-slate-400"/></button>
         </form>
           <NavLink className="hidden sm:inline hover:underline hover:cursor-pointer " to="/">Home</NavLink>
           <NavLink className="hidden sm:inline hover:underline hover:cursor-pointer " to ="/about">About</NavLink>
