783c33206e015c90b0b20a43f12eea1b4bbc98b1
Musariaz <riazmusa69@gmail.com>
2024-08-24 17:02:29 +0500

    About
    About page done

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About UrbanNest</h1>
+      <p className = 'mb-4 text-slate-700'>
+        Welcome to UrbanNest your go-to destination for finding the perfect
+        home. Whether you are looking to buy, rent, or explore exclusive offers,
+        UrbanNest is here to make your search simple and stress-free. Our
+        platform offers a diverse range of properties, from cozy apartments to
+        luxurious homes, all tailored to meet your unique needs.
+      </p>
+      <p className = 'mb-4 text-slate-700'>
+        At UrbanNest, we believe that finding a place to live should be more
+        than just a transaction—it should be an experience. Our user-friendly
+        platform allows you to browse through a vast selection of listings, each
+        carefully curated to help you find your next dream home. With powerful
+        search filters, detailed property descriptions, and high-quality images,
+        UrbanNest gives you all the tools you need to make an informed decision.
+      </p>
+      <h2 className="text-2xl text-slate-700 font-bold">Why Choose UrbanNest?</h2>
+      <p className = 'mt-4 mb-4 text-slate-700'>
+        Our database is constantly updated with new
+        properties, ensuring you have access to the latest options on the
+        market.
+        <br />
+      </p>
+      <p className = 'mb-4 text-slate-700'>
+        Designed with you in mind, UrbanNest’s intuitive interface
+        makes searching for properties quick and hassle-free.
+      </p>
+     
+      <p className = 'mb-4 text-slate-700'>
+        Our team is here to assist you at every step, providing
+        guidance and support as you navigate your real estate journey.
+      </p>
+      <br />
+      <p className = 'mb-4 text-slate-700'>Whether you’re a first-time homebuyer, a seasoned investor, or simply looking for a new rental, UrbanNest is here to help you find a place that feels like home. Start your search today and discover why thousands trust UrbanNest to find their perfect living space.</p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
